e7717d7e760d20962cab5246b8d441753efb4b6a
mitterdoo <connorashcroft@live.com>
2022-04-24 09:02:04 -0700

    Forgot to add license

--- lib/globals.lua
+++ lib/globals.lua
@@ -1,3 +1,22 @@
+--[[
+Hadron2D Game Engine for LÖVE
+Copyright (C) 2022  Connor Ashcroft
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program (see LICENSE.md).
+If not, see <https://www.gnu.org/licenses/>.
+]]
+
 ---Returns a percentage (0-1) of how far through a given time is through a specified range
 ---@param time number
 ---@param from number Start of range
